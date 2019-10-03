2431ba9e01bcfbf681e17cb3fdfe8d4dbb6e967f
Alexey Rusov <rolic402@mail.ru>
2019-10-03 12:05:08 +0300

    windows build.cmd aligned with linux Makefile

--- build.cmd
+++ build.cmd
@@ -19,13 +19,18 @@ set H=%H1%%H2%
 
 set BUILD_TIME=%Y%-%DATE:~3,2%-%DATE:~0,2%_%H%:%TIME:~3,2%:%TIME:~6,2%
 
-for /f %%i in ('type BUILD_NUMBER') do set "BUILD=%%i"
-set /a BUILD+=1
-echo %BUILD% >BUILD_NUMBER
+set BUILD=
+if exist BUILD_NUMBER (
+	for /f %%i in ('type BUILD_NUMBER') do set "BUILD=%%i"
+)
+if "%BUILD%"=="" (set BUILD=1)
 
 for /f %%i in ('type VERSION') do set "VERSION=%%i"
 set VERSION=%VERSION%.%BUILD%
 
-rem ® ¤ â  «®ª «ì­ ï,   ­¥ UTC
+rem ÐÐ¾ Ð´Ð°Ñ‚Ð° Ð»Ð¾ÐºÐ°Ð»ÑŒÐ½Ð°Ñ, Ð° Ð½Ðµ UTC
 
 go build -a --ldflags "-extldflags -static -X github.com/alrusov/misc.appVersion=%VERSION% -X github.com/alrusov/misc.buildTime=%BUILD_TIME% -X github.com/alrusov/misc.copyright=%COPYRIGHT%"
+
+set /a BUILD+=1
+echo %BUILD% >BUILD_NUMBER
