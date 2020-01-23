4c36e16b3cbc90f6df8e242a0644fad04c6008e5
Alexey Rusov <rolic402@mail.ru>
2020-01-23 17:18:14 +0300

    minor changes

--- build.cmd
+++ build.cmd
@@ -1,37 +1,48 @@
 @echo off
 
 if exist vendor\github.com (
-	echo Please delete the vendor\github.com directory
-	echo See https://github.com/golang/go/issues/19000 for details
-	exit
+  echo Please delete the vendor\github.com directory
+  echo See https://github.com/golang/go/issues/19000 for details
+  exit
 )
 
 set BUILD_OS=windows
 set BUILD_ARCH=x86_64
 set CGO_ENABLED=0 
+set EXTRA_LD=-extldflags -static
 
-set Y=%DATE:~6,4%
+for /f %%d in ('wmic path win32_utctime get /format:list ^| findstr "="') do (
+  for /f "tokens=1,2 delims==" %%a in ("%%d") do (
+    if "%%a" == "Year"   set Y=%%b
+    if "%%a" == "Month"  set N=%%b
+    if "%%a" == "Day"    set D=%%b
+    if "%%a" == "Hour"   set H=%%b
+    if "%%a" == "Minute" set M=%%b
+    if "%%a" == "Second" set S=%%b
+  )
+)
+if %N% lss 10 set N=0%N%
+if %D% lss 10 set D=0%D%
+if %H% lss 10 set H=0%H%
+if %M% lss 10 set M=0%M%
+if %S% lss 10 set S=0%S%
+
 set COPYRIGHT=(C)_Alexey_Rusov_(rolic402@mail.ru),_2017-%Y%
-
-set H1=%TIME:~0,1%
-set H2=%TIME:~1,1%
-if "%H1%"==" " (set H1=0)
-set H=%H1%%H2%
-
-set BUILD_TIME=%Y%-%DATE:~3,2%-%DATE:~0,2%_%H%:%TIME:~3,2%:%TIME:~6,2%
+set BUILD_TIME=%Y%-%N%-%D%_%H%:%M%:%S%
 
 set BUILD=
 if exist BUILD_NUMBER (
-	for /f %%i in ('type BUILD_NUMBER') do set "BUILD=%%i"
+  for /f %%i in ('type BUILD_NUMBER') do set "BUILD=%%i"
 )
 if "%BUILD%"=="" (set BUILD=1)
 
 for /f %%i in ('type VERSION') do set "VERSION=%%i"
 set VERSION=%VERSION%.%BUILD%
 
-echo Local time is not UTC ((
-
-go build -o cmd\%BUILD_OS%\%BUILD_ARCH%\service-template.exe --ldflags "-extldflags -static -X github.com/alrusov/misc.appVersion=%VERSION% -X github.com/alrusov/misc.buildTime=%BUILD_TIME% -X github.com/alrusov/misc.copyright=%COPYRIGHT%"
+go build ^
+  -o cmd\%BUILD_OS%\%BUILD_ARCH%\service-template.exe ^
+  --ldflags ^
+  "%EXTRA_LD% -X github.com/alrusov/misc.appVersion=%VERSION% -X github.com/alrusov/misc.buildTime=%BUILD_TIME% -X github.com/alrusov/misc.copyright=%COPYRIGHT%"
 
 set /a BUILD+=1
 echo %BUILD% >BUILD_NUMBER
--- go.mod
+++ go.mod
@@ -4,8 +4,8 @@ go 1.13
 
 require (
 	github.com/alrusov/config v0.1.5
-	github.com/alrusov/launcher v0.1.4
-	github.com/alrusov/misc v0.1.5
+	github.com/alrusov/launcher v0.1.5
+	github.com/alrusov/misc v0.1.6
 	github.com/alrusov/panic v0.1.3
 	github.com/alrusov/stdhttp v0.1.5
 )
